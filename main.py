588ec8ec465a615c708571ccdd7bac27dbe56175
AnuragsistaSS <anurag.sista@sticsoftsolutions.com>
2025-03-04 16:13:06 +0530

    CRUD operations using sqlalchemy

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from dotenv import load_dotenv
 from psycopg2.extras import RealDictCursor
 from psycopg2 import DatabaseError
 import models
-from models import Base
 from database import engine, SessionLocal, get_db
 from sqlalchemy.orm import Session
 app = FastAPI()
@@ -16,59 +15,28 @@ load_dotenv()
 
 models.Base.metadata.create_all(bind=engine)
 
-sample_data = [
-    {
-        "roll_no": 101,
-        "name": "Alice Johnson",
-        "courses": ["Mathematics", "Physics", "Computer Science"]
-    },
-    {
-        "roll_no": 102,
-        "name": "Bob Smith",
-        "courses": ["History", "Philosophy", "English"]
-    },
-    {
-        "roll_no": 103,
-        "name": "Charlie Brown",
-        "courses": ["Biology", "Chemistry", "Mathematics"]
-    },
-    {
-        "roll_no": 104,
-        "name": "David Clark",
-        "courses": ["Psychology", "Sociology", "Statistics"]
-    },
-    {
-        "roll_no": 105,
-        "name": "Eve White",
-        "courses": ["Art", "Music", "Literature"]
-    }
-]
-
 
 class Student(BaseModel):
-    #student details 
-    roll_no:int
-    name:str
-    courses: List[str]
+    roll_no : int 
+    name : str
+    enrolled_courses : List[str]
 
 
 # setting up databse conncetion 
 
-try:
-    Database_host = os.getenv("DATABASE_HOST")
-    Database_port = os.getenv("PORT")
-    Database_name = os.getenv('DATABASE')
-    User = os.getenv("USER")
-    Password = os.getenv("PASSWORD")
-    conn = psycopg2.connect(host = Database_host, port=Database_port, database = Database_name, user=User, password=Password, cursor_factory=RealDictCursor)
-    cursor = conn.cursor()
+# try:
+#     Database_host = os.getenv("DATABASE_HOST")
+#     Database_port = os.getenv("PORT")
+#     Database_name = os.getenv('DATABASE')
+#     User = os.getenv("USER")
+#     Password = os.getenv("PASSWORD")
+#     conn = psycopg2.connect(host = Database_host, port=Database_port, database = Database_name, user=User, password=Password, cursor_factory=RealDictCursor)
+#     cursor = conn.cursor()
 
-except Exception as error:
-    print("Failed to connect to database \n")
-    print(f"Error:{error}")
+# except Exception as error:
+#     print("Failed to connect to database \n")
+#     print(f"Error:{error}")
     
-
-
 def find_student(roll_no):
     index = 0 
     for data in sample_data:
@@ -79,79 +47,55 @@ def find_student(roll_no):
     return None
 
 @app.get("/")
-def get_all_students():
-    cursor.execute("""select * from public."Student Details" """)
-    all_details = cursor.fetchall()
-    if all_details is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No records available")
+def get_all_students( db: Session = Depends(get_db)):
+    all_students = db.query(models.Student).all()
+    if all_students is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"No records found")
 
-    return{"Student_details":all_details}
-
+    return {"Student Records": all_students}
+    
 
 @app.post("/add_student",status_code = status.HTTP_201_CREATED)
-def add_student(student:Student):
-    try:
+def add_student(student:Student, db: Session = Depends(get_db)):
+   
 
-        cursor.execute(""" insert into public."Student Details" (roll_no,name,courses) values(%s,%s,%s);
-                   select * from public."Student Details" """,(student.roll_no,student.name,student.courses))
-        conn.commit()
-        student_details = cursor.fetchall()
-        return {"student_details ": student_details}
-    except Exception as e:
-        #conn.rollback()
-        print("Error occurred:", e)
-        raise HTTPException(status_code=500, detail=f"Exception: {e}")
-        
-    
-    # student_details = student.model_dump()
-    # sample_data.append(student_details)
-    # print(sample_data)
+    new_student = models.Student(**student.model_dump())
+    db.add(new_student)
+    db.commit()
+    db.refresh(new_student)
+    return {"New student":new_student}
     
 
 @app.get("/get_student/{roll_no}")
-def get_student(roll_no: int):
-    cursor.execute(""" select from public."Student Details" where roll_no = %s""",str(roll_no))
-    student_detail = cursor.fetchone()
-    if student_detail is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Student not found")
+def get_student(roll_no: int, db: Session = Depends(get_db)):
 
-    return {f"Details of student with {roll_no}": student_detail}
-    # index = find_student(roll_no)
-    # if index is None:
-    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-    #                         detail = f"post with id: {roll_no} was not found")
-    # return {"student_details": sample_data[index]}
+    student = db.query(models.Student).filter(models.Student.roll_no==roll_no).first()
+    if not student:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"student with {roll_no} not found")
+
+    return {"student": student}    
+   
 
 # deleting a post 
 @app.delete("/get_student/{roll_no}", status_code=status.HTTP_204_NO_CONTENT)
-def del_student(roll_no: int ):
-    cursor.execute("""delete from public."Student Details" where roll_no = %s ;
-                   select * from public."Student Details" """, str(roll_no))
-    conn.commit()
+def del_student(roll_no: int,db: Session = Depends(get_db)):
+    
+    student_query = db.query(models.Student).filter(models.Student.roll_no==roll_no)
+    if not student_query.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"student with {roll_no} not found")
+    student_query.delete(synchronize_session= False)
+    db.commit()
 
-    # index = find_student(roll_no)
-    # if index is None:
-    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-    #                         detail = f"post with id: {roll_no} was not found")
-    # else:
-    #     sample_data.pop(index)
+    
 
 # updating a student
 @app.put("/get_student/{roll_no}", status_code=status.HTTP_201_CREATED)
-def updated_student(roll_no:int, new_details : Student):
-    # now working on ORM
-    cursor.execute(""" UPDATE public."Student Details" set roll_no = %s, name = %s, courses = %s where roll_no  = %s ;
-                   select * from public."Student Details" """ , (new_details.roll_no,new_details.name,new_details.courses, str(roll_no)))
-    updated_details = cursor.fetchall()
-    conn.commit()
-    return{"Updated Student details": updated_details}
+def updated_student(roll_no:int, new_details : Student, db: Session = Depends(get_db)):
+    student_query = db.query(models.Student).filter(models.Student.roll_no==roll_no)
+    if not student_query.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"student with roll no {roll_no} not found")
+    student_query.update(new_details.model_dump(), synchronize_session=False)
+    db.commit()
+    return{"message": f"Succesfully updated student with roll no {roll_no}"}
 
-
-    
-# checking sqlaclhemy functionality
-
-@app.get("/sqlalchemy")
-def test_students(db: Session = Depends(get_db)):
-    students = db.query(models.Student).all()
-    return {"Students": students}
-
+   
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, TIMESTAMP
 from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy.sql.expression import text
 
 class Student(Base):
     __tablename__= "Student_details" 
@@ -8,3 +9,4 @@ class Student(Base):
     roll_no = Column(Integer,primary_key = True, nullable= False)
     name = Column(String,nullable=False)
     enrolled_courses = Column(ARRAY(String), nullable=True)
+    added_at = Column(TIMESTAMP(timezone=True),nullable= False, server_default = text('now()'))
