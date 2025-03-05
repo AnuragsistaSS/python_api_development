79e0217861c7aa61aee13cec4efc77c0fa48a942
AnuragsistaSS <anurag.sista@sticsoftsolutions.com>
2025-03-05 15:41:58 +0530

    added token expiration functionality

--- oauth2.py
+++ oauth2.py
@@ -1,19 +1,21 @@
 from jose import JWTError, jwt
-from datetime import datetime, timedelta
-import schema
+from datetime import datetime, timedelta, timezone
+import schema, models
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-
+from sqlalchemy.orm import Session
+from database import get_db
 outh2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
 SECRET_KEY = "c9c15242bbd3fa96ee14bc766b750efffbdb39770b73a06c88b80a962e7684c9"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 1
 
 def create_access_token(data:dict):
     to_encode = data.copy()
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    print("expired:",expire)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
@@ -27,8 +29,10 @@ def verify_access_token(token:str , credentials_exception):
         token_data = schema.TokenData(id=user_id)
     except JWTError:
         raise credentials_exception
-    return user_id
+    return token_data
     
-def get_current_user(token:str = Depends(outh2_scheme)):
+def get_current_user(token:str = Depends(outh2_scheme),db:Session = Depends(get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Could not validate credentials",headers={"WWW-Authenticate":"Bearer"})
-    return verify_access_token(token, credentials_exception) 
+    token= verify_access_token(token, credentials_exception) 
+    user = db.query(models.Users).filter(models.Users.id==token.id).first()
+    return user
